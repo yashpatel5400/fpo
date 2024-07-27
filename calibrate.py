b158ba2c78ade950d14681e501dabdb1a293af56
yashpatel5400 <ypatel5400@gmail.com>
2024-07-27 11:27:23 -0400

    Add alternate score formulations to calibration and optimization

--- calibrate.py
+++ calibrate.py
@@ -62,7 +62,7 @@ def c3_metric(field):
     return np.sum(partial_field_maxes, axis=0)
 
 
-def get_scores(train_loader, fno, training_type):
+def get_scores(train_loader, fno, training_type, score_func):
     scores = []
     for xxbatch, yy, gridbatch in train_loader:
         if training_type == "autoregressive":
@@ -76,8 +76,14 @@ def get_scores(train_loader, fno, training_type):
         else:
             yyhat   = fno(xxbatch[...,0,:].to(device), gridbatch.to(device))
             yybatch = yy.to(device)
-        diff        = (yyhat[...,0,0] - yybatch[...,0,0]).cpu().detach().numpy()
-        score_batch = c3_metric(diff)
+        diff = (yyhat[...,0,0] - yybatch[...,0,0]).cpu().detach().numpy()
+
+        if score_func == "c3":
+            score_batch = c3_metric(diff)
+        elif score_func == "l2":
+            score_batch = np.linalg.norm(diff.reshape(diff.shape[0], -1), ord=2, axis=-1)
+        elif score_func == "linf":
+            score_batch = np.linalg.norm(diff.reshape(diff.shape[0], -1), ord=np.inf, axis=-1)
         scores.append(score_batch)
     return np.concatenate(scores)
 
@@ -103,7 +109,7 @@ def plot_calibration(alphas, ax, downsampling_to_cp_quantiles, downsampling_to_t
     ax.legend_ = None # have just a single legend for the figure
     
 
-def test_calibration(alphas, pde, ax):
+def test_calibration(alphas, score_func, pde, ax):
     cfg_fn = os.path.join("experiments", f"config_{pde}.yaml")
     with open(cfg_fn, "r") as f:
         cfg = yaml.safe_load(f)
@@ -124,7 +130,7 @@ def test_calibration(alphas, pde, ax):
         batch_size = 25
         train_data = FNODatasetSingle(filename=os.path.join("experiments", cfg["filename"]), reduced_resolution=downsampling)
         train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size)    
-        downsampling_to_scores[downsampling] = get_scores(train_loader, fno, cfg["training_type"])
+        downsampling_to_scores[downsampling] = get_scores(train_loader, fno, cfg["training_type"], score_func)
 
     downsampling_to_cp_quantiles = {}
     downsampling_to_test_scores  = {}
@@ -147,19 +153,20 @@ def test_calibration(alphas, pde, ax):
 
 if __name__ == "__main__":
     os.makedirs("results", exist_ok=True)
-    fig, axs = plt.subplots(1, 3, figsize=(18,6))
-    for pde, ax in zip(["darcy", "diffreact", "rdb"], axs):
-        print(f"Calibrating: {pde}")
-        alphas = np.arange(0, 1, 0.05)
-        pde_cp_quantiles = test_calibration(alphas, pde, ax)
+    for score_func in ["c3", "l2", "linf"]:
+        fig, axs = plt.subplots(1, 3, figsize=(18,6))
+        for pde, ax in zip(["darcy", "diffreact", "rdb"], axs):
+            print(f"Calibrating {score_func}: {pde}")
+            alphas = np.arange(0, 1, 0.05)
+            pde_cp_quantiles = test_calibration(alphas, score_func, pde, ax)
 
-        result_fn = os.path.join("experiments", f"quantiles_{pde}.csv")
-        scores_df = pd.DataFrame.from_dict(pde_cp_quantiles).set_index(alphas)
-        scores_df.to_csv(result_fn)
+            result_fn = os.path.join("experiments", f"quantiles_{pde}_{score_func}.csv")
+            scores_df = pd.DataFrame.from_dict(pde_cp_quantiles).set_index(alphas)
+            scores_df.to_csv(result_fn)
 
-    axs[0].set_ylabel(r"$\mathrm{Empirical\ Coverage}$")
-    handles, labels = axs[-1].get_legend_handles_labels()
-    fig.legend(handles, labels, loc='upper right')
-    fig.tight_layout()
-    
-    plt.savefig(os.path.join("results", "calibration.png"))
+        axs[0].set_ylabel(r"$\mathrm{Empirical\ Coverage}$")
+        handles, labels = axs[-1].get_legend_handles_labels()
+        fig.legend(handles, labels, loc='upper right')
+        fig.tight_layout()
+        
+        plt.savefig(os.path.join("results", f"calibration_{score_func}.png"))
--- fpo.py
+++ fpo.py
@@ -17,7 +17,7 @@ from fno_utils import FNO2d, FNODatasetSingle
 from wavelet import WaveletBasis, get_disc_grid    
 
 
-def optimize(uhat, quantile, r):
+def optimize(uhat, quantile, r, score_func):
     """Primary driver function of FPO pipeline -- solves the robust problem of min_{w} max_{u in B_q(uhat)} J(u, w)
     for J of the form prescribed in the paper and the ball defined in C^3 norm. 
 
@@ -26,6 +26,7 @@ def optimize(uhat, quantile, r):
         quantile -- Radius of the ball (in C^3 norm). If given is None, the nominal solution is 
             provided instead (i.e. simply taking the prescribed uhat to be true)
         r -- Radius of the "collector" in the problem specification
+        score_func -- Name of score function to use for the constraint (can be None if quantile is None)
     """
     wavelet_basis = WaveletBasis()
 
@@ -40,30 +41,42 @@ def optimize(uhat, quantile, r):
     x_grid = wavelet_basis.x_grid.reshape(uhat.shape[0], uhat.shape[1], -1)
 
     # problem specification (constant over optimization)
-    eta       = 1e-3
+    eta       = 1e-3 if score_func != "c3" else 5e-4
     max_iters = 1_000
 
     w = np.array([0.6, 0.6])
     for iter in range(max_iters):
         # ---- Compute u^* using parametric wavelet formulation in robust formulation
         if quantile is not None:
-            u_coeff = cp.Variable(uhat_coeffs.shape)
+            if score_func == "c3":
+                u_coeff = cp.Variable(uhat_coeffs.shape)
 
-            integral_mask = (np.linalg.norm(wavelet_basis.x_grid - w, axis=1) < r).astype(np.int8).reshape(uhat.shape)
-            psi_w = np.sum(shaped_basis * integral_mask, axis=(1,2))
-            objective = cp.Minimize(-u_coeff @ psi_w)
+                integral_mask = (np.linalg.norm(wavelet_basis.x_grid - w, axis=1) < r).astype(np.int8).reshape(uhat.shape)
+                psi_w = np.sum(shaped_basis * integral_mask, axis=(1,2))
+                objective = cp.Minimize(-u_coeff @ psi_w)
 
-            constraints = [
-                cp.sum([
-                    cp.max(cp.hstack([
-                        cp.abs(partial @ (uhat_coeffs - u_coeff)) for partial in partial_order
-                    ])) for partial_order in partials
-                ]) <= quantile
-            ]
+                constraints = [
+                    cp.sum([
+                        cp.max(cp.hstack([
+                            cp.abs(partial @ (uhat_coeffs - u_coeff)) for partial in partial_order
+                        ])) for partial_order in partials
+                    ]) <= quantile
+                ]
 
-            prob = cp.Problem(objective, constraints)
-            obj  = prob.solve()
-            u_star = (wavelet_basis.basis_func @ u_coeff.value).reshape(32, 32)
+                prob = cp.Problem(objective, constraints)
+                obj  = prob.solve()
+                u_star = (wavelet_basis.basis_func @ u_coeff.value).reshape(uhat.shape)
+            else:
+                yhat = uhat.flatten()
+                w_mask = (np.linalg.norm(wavelet_basis.x_grid - w, axis=1) < r).astype(np.int8)
+                norm_ord = 2 if score_func == "l2" else "inf"
+
+                u = cp.Variable(yhat.shape)
+                objective = cp.Minimize(-u @ w_mask)
+                constraints = [cp.norm(u - yhat, norm_ord) <= quantile]
+                prob = cp.Problem(objective, constraints)
+                obj  = prob.solve()
+                u_star = u.value.reshape(uhat.shape)
         else:
             u_star = uhat
 
@@ -123,8 +136,8 @@ def fpo(cfg):
         uhat = uuhat[0,...,0,0].detach().cpu().numpy()
         u    = uubatch[0,...,0,0].detach().cpu().numpy()
         
-        w = optimize(uhat, 1.0, r)
-        w_star = optimize(u, None, r)
+        w = optimize(uhat, cfg["cp_quantile"], r, cfg["score_func"])
+        w_star = optimize(u, None, r, None)
         regret_ratios.append(eval_regret_ratio(u, w, w_star, r))
     return np.array(regret_ratios)
 
@@ -132,6 +145,7 @@ def fpo(cfg):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--pde", choices=["darcy", "diffreact", "rdb"])
+    parser.add_argument("--score_func", choices=["c3", "l2", "linf"])
     parser.add_argument("--nominal", action="store_true")
     parser.add_argument("--downsampling", type=int, default=1)
     args = parser.parse_args()
@@ -139,16 +153,17 @@ if __name__ == "__main__":
     cfg_fn = os.path.join("experiments", f"config_{args.pde}.yaml")
     with open(cfg_fn, "r") as f:
         cfg = yaml.safe_load(f)
+    cfg["score_func"]   = args.score_func
     cfg["downsampling"] = args.downsampling
 
     if args.nominal:
         cfg["cp_quantile"] = None
     else:
         alpha = 0.95
-        cp_quantiles_fn = os.path.join("experiments", f"quantiles_{args.pde}.csv")
-        cfg["cp_quantile"] = pd.read_csv(cp_quantiles_fn)[str(cfg["downsampling"])].values[-1]
+        cp_quantiles_fn = os.path.join("experiments", f"quantiles_{args.pde}_{args.score_func}.csv")
+        cfg["cp_quantile"] = pd.read_csv(cp_quantiles_fn)[str(cfg["downsampling"])].values[1]
     regret_ratios = fpo(cfg)
 
-    result_fn = os.path.join("results", f"regret_{args.pde}-{args.nominal}.pkl")
+    result_fn = os.path.join("results", f"regret_{args.pde}_{args.score_func}.pkl")
     with open(result_fn, "wb") as f:
         pickle.dump(regret_ratios, f)
